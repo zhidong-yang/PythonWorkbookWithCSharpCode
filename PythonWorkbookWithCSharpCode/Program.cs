4a248e212e522f9d2aa88e336f0252d9daf88175
zhidong-yang <zhidong.yang@outlook.com>
2020-08-13 23:19:44 -0400

    Add project files.

---
+++ PythonWorkbookWithCSharpCode/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythonWorkbookInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //##################### START OF EXERCISE 11 FUEL EFFICIENCY ################################
+        public void MPGToLPH(double mpg)
+        {
+            // 1 mile = 1.6km
+            // 1 gallon = 3.79 liters
+            // 1hpl = 1000km/1L = (1000/1.6)mile / (1/3.79)gallon
+            // 1LPH = 1/1hpl
+
+            double LPH = 1 / (mpg / ((1000 / 1.6) / (1 / 3.79)));
+        }
+        //##################### END OF EXERCISE 11 FUEL EFFICIENCY ###################################
+
+
+        //##################### START OF EXERCISE 48 CHINESE ZODIAC ################################
+        public void ChineseZodiac(int year)
+        {
+            //2000 % 12 = 8, which makes year 0 Year Monkey.
+            int yearResidual = year % 12;
+            switch (yearResidual)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Monkey");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Rooster");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Dog");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Pig");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Rat");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Ox");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Tiger");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Hare");
+                        break;
+                    }
+                case 8:
+                    {
+                        Console.WriteLine("Dragon");
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.WriteLine("Snake");
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.WriteLine("Horse");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Sheep");
+                        break;
+                    }
+            }
+
+        }
+
+        //##################### END OF EXERCISE 48 CHINESE ZODIAC ###################################
+
+        //##################### START OF EXERCISE 69 APPROXIMATE PAI ################################
+        public void ApproxPai(int num)
+        {
+            // num is how many approximation of pai desired. In original question it requires to display all 15
+            // Here I display only one based on input
+            double outupt = 3.0;
+            for (int i = 1; i < num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    outupt += (4 / ((2 * i) * (2 * (i + 1)) * (2 * (i + 2))));
+                }
+                else
+                {
+                    outupt -= (4 / ((2 * i) * (2 * (i + 1)) * (2 * (i + 2))));
+                }
+            }
+
+            Console.WriteLine($"The {num}th approximation of Pai is {outupt}.");
+        }
+
+        //##################### END OF EXERCISE 69 APPROXIMATE PAI ###################################
+    }
+}
---
+++ PythonWorkbookWithCSharpCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PythonWorkbookWithCSharpCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PythonWorkbookWithCSharpCode")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cf8a6c9-62cf-426f-82f1-891c68f07ef5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
