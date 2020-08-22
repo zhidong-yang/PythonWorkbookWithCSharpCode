a3bd73cdfe7b72d698b1162012570e715db70fb0
zhidong-yang <zhidong.yang@outlook.com>
2020-08-22 09:49:33 -0400

    Q#94, Aug 22

--- PythonWorkbookProj2/Program.cs
+++ PythonWorkbookProj2/Program.cs
@@ -10,6 +10,30 @@ namespace PythonWorkbookProj2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(RandomPW());            
+
+            Console.ReadLine();
         }
+
+        //##################### START OF EXERCISE 94 RANDOM PASSWORD ################################
+        public static string RandomPW()
+        {
+            Random rdn = new Random();
+            // generate the number of characters in the output
+            int charNum = rdn.Next(7, 11);
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < charNum; i++)
+            {
+                // randomly generating an ASCII number based on question's requirement, then casting it to char
+                int ascii = rdn.Next(33, 127);
+                char asciiCasted = (char)ascii;
+                password.Append(asciiCasted);
+            }
+
+            string output = password.ToString();
+
+            return output;
+        }
+        //###################### END OF EXERCISE 94 RANDOM PASSWORD #################################
     }
 }
